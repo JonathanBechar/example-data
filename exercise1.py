5ebe93521e443ab5e9ecdf17b9c0ee5ddaf71dd5
JonathanBechar <yonatan.bechar@gmail.com>
2026-02-10 11:57:41 +0200

    added terminate function

--- exercise1.py
+++ exercise1.py
@@ -1,5 +1,6 @@
 import boto3
 from dotenv import load_dotenv
+import argparse
 
 load_dotenv()
 
@@ -26,18 +27,35 @@ client = boto3.client("ec2", region_name="us-east-1")
 def create_default_instance(instance_type: str):
     return client.run_instances(
         ImageId="ami-0c1fe732b5494dc14",
-        InstanceType="t2.micro",
+        InstanceType=instance_type,
         MaxCount=1,
         MinCount=1,
         SubnetId="subnet-02e105184e1285a86",
     )
 
-for instance_type in ["t2.micro", "t2.small", "t2.medium"]:
-    response = create_default_instance(instance_type)
+def solve_homework():
+    for instance_type in ["t2.micro", "t2.small", "t2.medium"]:
+        response = create_default_instance(instance_type)
+    last_id = response["Instances"][0]["InstanceId"]
+    client.get_waiter("instance_running").wait(InstanceIds=[last_id])
+    client.stop_instances(InstanceIds=[last_id])
 
-last_id = response["Instances"][0]["InstanceId"]
-client.get_waiter("instance_running").wait(InstanceIds=[last_id])
-client.stop_instances(InstanceIds=[last_id])
+def terminate_all_ec2():
+    response = client.describe_instances()
+    ids = []
+    for res in response["Reservations"]:
+        for instance in res["Instances"]:
+            ids.append(instance["InstanceId"])
+    client.terminate_instances(InstanceIds=ids)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--terminate", action="store_true")
+    args = parser.parse_args()
+    if args.terminate:
+        terminate_all_ec2()
+    else:
+        solve_homework()
 
 # stop_ids = []
 # while len(stop_ids) == 0:
